32cf7c2b2317cf706f75a8eb1c2590bfd622c889
lesha13 <leshamashuna@gmail.com>
2022-11-04 22:46:55 +0300

    update

--- lab4/Display.php
+++ lab4/Display.php
@@ -9,8 +9,14 @@ class Display
         $table .= '<tr> <th>id</th> <th>name</th> <th>phone</th> <th>address</th> <th>time</th> <th>bill</th> </tr>';
 
         foreach ($array as $item) {
-            $table .= "<tr><td>$item->id</td><td>$item->name</td><td>$item->phone</td>" .
-                "<td>$item->address</td><td>$item->time</td><td>$item->bill</td></tr>";
+            $table .=
+                "<tr><td>" . $item['id'] .
+                "</td><td>" .$item['name'] .
+                "</td><td>" . $item['address'] .
+                "</td><td>" . $item['phone'] .
+                "</td><td>" . $item['time'] .
+                "</td><td>" . $item['bill'] .
+                "</td></tr>";
         }
 
         $table .= '</table>';
--- lab4/Repository.php
+++ lab4/Repository.php
@@ -2,16 +2,42 @@
 
 class Repository
 {
-    public static function saveClients($array)
+    public $dbh;
+    public function __construct($dbh)
     {
-        $file = fopen("clients.txt", "w");
-        fwrite($file, serialize($array));
-        fclose($file);
+        $this->dbh = $dbh;
     }
 
-    public static function loadClients()
+    public function createClient($array)
     {
-        return unserialize(file_get_contents("clients.txt"));
+        $this->dbh->query('INSERT INTO Clients(name, address, phone, time, bill) VALUES (' .
+            "'" . $array['name'] . "', " .
+            "'" . $array['address'] . "', " .
+            "'" . $array['phone'] . "', " .
+            "'" . $array['time'] . "', " .
+            "'" . $array['bill'] . "')"
+        );
+    }
+
+    public function readClients()
+    {
+        return $this->dbh->query('SELECT * FROM Clients')->fetchAll();
+    }
+
+    public function updateClient($array)
+    {
+        $this->dbh->query('UPDATE Clients SET ' .
+            'name = ' . $array['name'] . ', ' .
+            'address = ' . $array['address'] . ', ' .
+            'phone = ' . $array['phone'] . ', ' .
+            'time = ' . $array['time'] . ' , ' .
+            'bill = ' . $array['bill'] . ' ' .
+            'WHERE id = ' . $array['id']);
+    }
+
+    public function deleteClient($array)
+    {
+        return $this->dbh->query('DELETE FROM Clients WHERE id = ' . $array['id']);
     }
 }
 
